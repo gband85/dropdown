0d668faaaef47f250db5ad52d0219ff3abe2a12e
gband85 <garrett@gband.net>
2022-05-24 19:51:20 -0500

    change class name, div->nav

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,19 +3,23 @@ const dropdown=(...theItems)=>{
 //first arg is logo text,1 to end is buttons text
 let itemsArr=theItems.slice(1);
 //create div
-    const dropdownUI=document.createElement("div");
+    const dropdownUI=document.createElement("nav");
+    dropdownUI.className="navbar"
     //create navbuttons
     const navButtons=document.createElement("div");
     navButtons.className="nav-buttons";
     navButtons.innerHTML=`<a href="index.html" class="logo">${theItems[0]}</a>
-  <i id="hamburger" class="fa-solid fa-bars"></i>`
+    <svg style="width:24px;height:24px" viewBox="0 0 24 24" class="hamburger">
+    <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+</svg>`
   dropdownUI.appendChild(navButtons);
   const navList=document.createElement("ul");
   navList.className="nav-list";
   //iterate through args,create li with button with arg
     itemsArr.forEach(function(item){
 const listItem=document.createElement("li");
-listItem.innerHTML=`<button class="btn">${item}</button>`
+listItem.className="menu-item";
+listItem.innerHTML=`<button class="btn">${item}</button>`;
 navList.appendChild(listItem);
     })
 dropdownUI.appendChild(navList);
